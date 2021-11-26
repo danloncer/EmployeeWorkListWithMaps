a64b867faaa26c5d6bad72781a3f993b5454453b
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-11-26 20:37:44 +0300

    Initial commit

---
+++ src/main/java/pro/sky/EmployeeWorkListWithMaps/EmployeeWorkListWithMapsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeWorkListWithMaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWorkListWithMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWorkListWithMapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/EmployeeWorkListWithMaps/EmployeeWorkListWithMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeWorkListWithMaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWorkListWithMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
